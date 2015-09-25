d0b85aab2dbfac7cc8bc9fa45fa1727ff8a687fa
Yuriy Linnyk <yuraji@gmail.com>
2015-09-25 23:09:29 +0300

    Admin Tweet Preview - Javascript

--- assets/js/ca-clicktotweet-plugin.js
+++ assets/js/ca-clicktotweet-plugin.js
@@ -2,18 +2,75 @@
     tinymce.create('tinymce.plugins.CAClickToTweet', {
         init: function(ed, url) {
             console.log( url );
+
+            jQuery('<link rel="stylesheet" src="../wp-content/plugins/Easy-Click-To-Tweet/assets/css/ca_click_to_tweet.css"></link>').appendTo('head');
+
+            var maxLength = 30; // maximum allowed characters
+
+            var $input; // tweet input
+            var $preview = jQuery('<div id="CAClickToTweetPreview" class="basic-white" />'); // preview container
+            var $counter = jQuery('<div id="CAClickToTweetCounter"/>'); // counter element
+            var $body; // popup body
+            var len; // length of the tweet
+            var danger; // maximum allowed characters exceeded
+
+            // temporary add style
+            $preview.add($counter).css({
+                position: 'absolute', bottom: 0, border: '5px solid yellow', padding: '5px'
+            });
+            $counter.css({ right: 0 });
+
+            /**
+             * Update the preview text
+             */
+            var updatePreview = function(){
+                var text = jQuery.trim( $input.val() );
+                if( !text ){
+                    text = "&nbsp;";
+                }
+                $preview.html( text );
+            };
+
+            /**
+             * Update the counter element with the number of entered tweet characters
+             */
+            var updateCounter = function(){
+                var num = $input.val().length;
+                $counter.html( num );
+                danger = (num>maxLength);
+                danger? $body.addClass('danger') : $body.removeClass('danger');
+            };
+
             ed.addButton('ca-clicktotweet', {
                 title: CAClickToTweet.button_title,
                 image: CAClickToTweet.url + '/assets/img/twitter-little-bird-button.png',
                 onclick: function() {
+
+                    /**
+                     * Setup element references after popup is opened.
+                     */
+                    var initPreview = function(e){
+                        initPreview = function(){}; /* make this function only runs once after popup is opened */
+                        $input = jQuery(e.target);
+                        $body = $input.closest('.mce-window-body');
+                        $body.append( $preview, $counter );
+                    };
+
                     ed.windowManager.open({
                         title: 'Easy Click To Tweet Boxes',
+                        width: 700,
+                        minHeight: 300,
                         body: [
                             {
                                 type   : 'textbox',
                                 name   : 'message',
                                 label  : 'Tweet text',
-                                minHeight: '50px'
+                                minHeight: '50px',
+                                onkeyup: function(e){
+                                    initPreview(e);
+                                    updatePreview();
+                                    updateCounter();
+                                }
                             }, {
                                 type   : 'listbox',
                                 name   : 'theme',
@@ -22,7 +79,10 @@
                                     { text: 'Basic White Skin', value: 'basic-white', selected: true },
                                     { text: 'Basic Full Skin', value: 'basic-full' },
                                     { text: 'Basic Border', value: 'basic-border' }
-                                ]
+                                ],
+                                onselect: function(e){
+                                    $preview.removeClass().addClass( this.value() );
+                                }
                             }
                         ],
                         onsubmit: function(e) {
